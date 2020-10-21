95abe139277d3f83c14797684e5693db73aab0e9
Shung-2 <shung_@kakao.com>
2020-10-21 23:55:32 +0900

    Add Part1_Section7_4

---
+++ CSharp/Part1_Section7_4.Delegate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    class Part1_Section7_4
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
