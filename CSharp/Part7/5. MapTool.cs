6f0bbbc49d215fa1dac70411191b00b328dfc728
Shung-2 <shung_@kakao.com>
2021-04-07 14:46:07 +0900

    210407 stduy content

---
+++ CSharp/Part7/5. MapTool.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.IO;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MapEditor
+{
+#if UNITY_EDITOR
+
+    // 단축키
+    // % (ctrl)의미, # (Shift), & (Alt)
+    [MenuItem("Tools/GenerateMap %#g")]
+    private static void GenerateMap()
+    {
+        GameObject[] gameObjects = Resources.LoadAll<GameObject>("Prefabs/Map");
+
+        foreach (GameObject go in gameObjects)
+        {
+            Tilemap tm = Util.FindChild<Tilemap>(go, "Tilemap_Collision", true);
+
+            // 파일을 만들어 기능을 추출하는 작업
+            // 바이너리 방식과, txt 방식이 있음.
+            using (var writer = File.CreateText($"Assets/Resources/Map/{go.name}.txt"))
+            {
+                writer.WriteLine(tm.cellBounds.xMin);
+                writer.WriteLine(tm.cellBounds.xMax);
+                writer.WriteLine(tm.cellBounds.yMin);
+                writer.WriteLine(tm.cellBounds.yMax);
+
+                for (int y = tm.cellBounds.yMax; y >= tm.cellBounds.yMin; y--)
+                {
+                    for (int x = tm.cellBounds.xMin; x <= tm.cellBounds.xMax; x++)
+                    {
+                        TileBase tile = tm.GetTile(new Vector3Int(x, y, 0));
+                        if (tile != null)
+                            writer.Write("1");
+                        else
+                            writer.Write("0");
+                    }
+                    writer.WriteLine();
+                }
+            }
+        }        
+    }
+#endif
+}
